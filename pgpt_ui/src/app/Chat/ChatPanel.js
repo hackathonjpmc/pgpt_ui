4055e68d240fad0c52194282d57a480b454eb519
alewoo28 <alexiswoodbury8@gmail.com>
2023-06-16 03:36:54 -0400

    dynamic clients, empty chat

--- pgpt_ui/src/app/Chat/ChatPanel.js
+++ pgpt_ui/src/app/Chat/ChatPanel.js
@@ -1,42 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Layout, Space, Input, Divider, List, Card, Image } from 'antd';
 import ViewChat from '@/app/Chat/ViewChat';
-import textForChat from '../mockChatText';
+import { textForChat } from '../mockChatText';
 
 const { Header, Footer, Sider, Content } = Layout;
 
 const { Search } = Input;
 
-const steps = [
-	{
-		id: '0',
-		message: 'Welcome to react chatbot!',
-		trigger: '1',
-	},
-	{
-		id: '1',
-		message: 'Bye!',
-		end: true,
-	},
-];
-
 const dividerStyle = {
 	margin: '5px',
-	// paddingBottom: '0',
 };
 
-export default function ChatPanel() {
+export default function ChatPanel({ currentService }) {
 	const [userInput, setUserInput] = useState('');
-	const [previousMessages, setPreviousMessages] = useState([]);
+	const [chatHistory, setChatHistory] = useState([]);
 	const [messageIDs, setMessageIDs] = useState(0);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const response = textForChat;
+				const response = currentService === '0' ? textForChat : [];
 				// const response = await fetch('someEndpoint');
 				// const json = await response.json();
-				setPreviousMessages(response);
+				setChatHistory(response);
+				setMessageIDs(response.length);
 			} catch (error) {
 				console.error('Error:', error);
 			}
@@ -45,23 +32,27 @@ export default function ChatPanel() {
 		fetchData();
 	}, []);
 
-	// const handleChange = (e) => {
-	// 	setUserInput(e.target.value);
-	// };
+	useEffect(() => {
+		const response = currentService === '0' ? textForChat : [];
+		setChatHistory(response);
+	}, [currentService]);
 
 	const handleSearch = () => {
+		const convertID = Number(messageIDs);
+
 		const newItem = {
 			id: messageIDs, // using the length of current data as id
 			message: userInput, // using the length of current data as message
 			user: true, // replace this with the actual username
 		};
-		setPreviousMessages((prevData) => [...prevData, newItem]);
+		setChatHistory((prevData) => [...prevData, newItem]);
 		setUserInput('');
+		setMessageIDs(messageIDs + 1);
 	};
 
 	return (
 		<div>
-			<ViewChat output={previousMessages} />
+			<ViewChat chatHistory={chatHistory} />
 			<Divider style={dividerStyle} />
 			<Search
 				placeholder='ask Payment GPT'
--- pgpt_ui/src/app/mockChatText.js
+++ pgpt_ui/src/app/mockChatText.js
@@ -1,74 +1,99 @@
-const textForChat = [
+export const textForChat = [
 	{
 		id: 1,
-		message: 'placeholder',
+		message: 'Tulpa qui officia deserunt mollit anim id est laborum.',
 		user: false,
 	},
 	{
 		id: 2,
-		message: 'lorem ipsum dolor sit. lorem ipsum dolor. lorem ipsum dolor',
+		message:
+			'lorem ipsum dolor sit. lorem ipsum dolor. Culpa qui officia deserunt mollit anim id est laborum. lorem ipsum dolor',
 		user: false,
 	},
 	{
 		id: 3,
-		message: 'placeholder',
+		message:
+			'Nam libro tempore. Ut aliquip ex ea commodo consequat. Duis aute irure dolor ',
 		user: false,
 	},
 	{
 		id: 4,
-		message: 'lorem ipsum dolor sit. lorem ipsum dolor. lorem ipsum dolor',
+		message:
+			'lorem ipsum dolor sit. lorem ipsum dolor. nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. lorem ipsum dolor',
 		user: false,
 	},
 	{
 		id: 5,
-		message: 'placeholder',
-		user: false,
+		message:
+			'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis. Ut aliquip ex ea commodo consequat. Duis aute irure dolor ',
+		user: true,
 	},
 	{
 		id: 6,
-		message: 'lorem ipsum dolor sit. lorem ipsum dolor. lorem ipsum dolor',
+		message:
+			'Nemo enim ipsam voluptatem quia voluptas. Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores',
+		user: false,
+	},
+	{
+		id: 7,
+		message:
+			'Culpa qui officia deserunt mollit. Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Anim id est laborum.',
 		user: true,
 	},
-	// {
-	// 	id: 7,
-	// 	message: 'placeholder',
-	// 	user: false,
-	// },
-	// {
-	// 	id: 8,
-	// 	message: 'lorem ipsum dolor sit. lorem ipsum dolor. lorem ipsum dolor',
-	// 	user: false,
-	// },
-	// {
-	// 	id: 9,
-	// 	message: 'placeholder',
-	// 	user: false,
-	// },
-	// {
-	// 	id: 10,
-	// 	message: 'lorem ipsum dolor sit. lorem ipsum dolor. lorem ipsum dolor',
-	// 	user: true,
-	// },
-	// {
-	// 	id: 11,
-	// 	message: 'placeholder',
-	// 	user: false,
-	// },
-	// {
-	// 	id: 12,
-	// 	message: 'lorem ipsum dolor sit. lorem ipsum dolor. lorem ipsum dolor',
-	// 	user: false,
-	// },
-	// {
-	// 	id: 13,
-	// 	message: 'placeholder',
-	// 	user: false,
-	// },
-	// {
-	// 	id: 14,
-	// 	message: 'lorem ipsum dolor sit. lorem ipsum dolor. lorem ipsum dolor',
-	// 	user: true,
-	// },
+	{
+		id: 8,
+		message: 'Lorem ipsum dolor sit. lorem ipsum dolor. lorem ipsum dolor',
+		user: false,
+	},
 ];
 
-export default textForChat;
+const textForChat2 = [
+	{
+		id: 1,
+		message: 'Tulpa qui officia deserunt mollit anim id est laborum.',
+		user: false,
+	},
+	{
+		id: 2,
+		message:
+			'lorem ipsum dolor sit. lorem ipsum dolor. Culpa qui officia deserunt mollit anim id est laborum. lorem ipsum dolor',
+		user: false,
+	},
+	{
+		id: 3,
+		message:
+			'Nam libro tempore. Ut aliquip ex ea commodo consequat. Duis aute irure dolor ',
+		user: false,
+	},
+	{
+		id: 4,
+		message:
+			'lorem ipsum dolor sit. lorem ipsum dolor. nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. lorem ipsum dolor',
+		user: false,
+	},
+	{
+		id: 5,
+		message:
+			'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis. Ut aliquip ex ea commodo consequat. Duis aute irure dolor ',
+		user: true,
+	},
+	{
+		id: 6,
+		message:
+			'Nemo enim ipsam voluptatem quia voluptas. Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores',
+		user: false,
+	},
+	{
+		id: 7,
+		message:
+			'Culpa qui officia deserunt mollit. Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Anim id est laborum.',
+		user: true,
+	},
+	{
+		id: 8,
+		message: 'Lorem ipsum dolor sit. lorem ipsum dolor. lorem ipsum dolor',
+		user: false,
+	},
+];
+
+export default { textForChat, textForChat2 };
